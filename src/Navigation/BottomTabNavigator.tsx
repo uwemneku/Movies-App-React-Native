e626acfedaa912d7c4ebe0d689753b96395dca2c
Uwem Israel <iamuwem@gmail.com>
2021-12-27 16:50:24 +0100

    fininshed custom bottom tab

--- src/Navigation/BottomTabNavigator.tsx
+++ src/Navigation/BottomTabNavigator.tsx
@@ -3,17 +3,26 @@ import {
   createBottomTabNavigator,
 } from "@react-navigation/bottom-tabs";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Dimensions,
+  Pressable,
+  StyleSheet,
+  Text,
+  useWindowDimensions,
+  View,
+} from "react-native";
 import { Bookmarks, Cart, Home, Settings } from "../Screens";
 import { BottomTabParamList } from "./types";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const { Navigator, Screen } = createBottomTabNavigator<BottomTabParamList>();
 const BottomTabNavigator = () => {
   return (
     <Navigator
-      tabBar={TabBar}
+      tabBar={(props) => <TabBar {...props} />}
       initialRouteName="Home"
       screenOptions={{ headerShown: false }}
+      sceneContainerStyle={{ backgroundColor: "#fff" }}
     >
       <Screen name="Home" component={Home} />
       <Screen name="Cart" component={Cart} />
@@ -26,27 +35,54 @@ const BottomTabNavigator = () => {
 export default BottomTabNavigator;
 
 const TabBar = ({ navigation, state }: BottomTabBarProps) => {
+  const { width } = useWindowDimensions();
   return (
     <View style={styles.container}>
-      <View style={styles.tabBar}>
-        <Text>{"dddd"}</Text>
+      <View style={[styles.tabBar, { width: width - 40 }]}>
+        {state.routes.map((route, index) => {
+          const isFocused = index === state.index;
+          const onPress = () => {
+            navigation.navigate(route.name);
+          };
+          return (
+            <Pressable key={route.name} {...{ onPress }}>
+              <Ionicons
+                name={icons[index]}
+                size={30}
+                color={"black"}
+                style={{ opacity: isFocused ? 1 : 0.2 }}
+              />
+            </Pressable>
+          );
+        })}
       </View>
     </View>
   );
 };
-
+const icons = [
+  "home-outline",
+  "bookmark-outline",
+  "cart-outline",
+  "settings-outline",
+];
 const styles = StyleSheet.create({
   container: {
+    bottom: 0,
     paddingHorizontal: 20,
-    marginBottom: 20,
+    paddingBottom: 20,
+    backgroundColor: "transparent",
+    position: "absolute",
+    alignItems: "center",
+    justifyContent: "center",
   },
   tabBar: {
     padding: 20,
-    // paddingHorizontal: 20,
     backgroundColor: "white",
     borderRadius: 15,
     marginBottom: 10,
     marginVertical: 20,
-    elevation: 0.5,
+    elevation: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
   },
 });
